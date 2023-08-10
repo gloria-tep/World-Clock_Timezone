f0f081be7832071662efc3434c34f00536c99f42
gloria-tep <tep.gloria18@gmail.com>
2023-08-10 18:16:25 +0100

    updated intervals

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
-let londonCityElement = document.querySelector("#Ldn-city");
-let londonDateElement = londonCityElement.querySelector(".date");
-let londonTimeElement = document.querySelector(".time");
-let londonTime = moment().tz("Europe/London");
+function updateInterval() {
+  let londonElement = document.querySelector("#London");
+  let londonDateElement = londonElement.querySelector("#london-date");
+  let londonTimeElement = document.querySelector("#london-time");
+  let londonTime = moment().tz("Europe/London");
 
-londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
-londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>]A[</small]");
-//Tokyo
-let tokyoCityElement = document.querySelector("#Tokyo-city");
-let tokyoDateElement = tokyoCityElement.querySelector(".date");
-let tokyoTimeElement = document.querySelector(".time");
-let tokyoTime = moment().tz("Asia/Tokyo");
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "h:mm:ss [<small>]A[</small]"
+  );
 
-tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
-tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
-//New york
-let nyCityElement = document.querySelector("#Ny-city");
-let nyDateElement = nyCityElement.querySelector(".date");
-let nyTimeElement = document.querySelector(".time");
-let nyTime = moment().tz("America/New_York");
+  let tokyoElement = document.querySelector("#Tokyo");
+  let tokyoDateElement = tokyoElement.querySelector("#tokyo-date");
+  let tokyoTimeElement = document.querySelector("#tokyo-time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
 
-nyDateElement.innerHTML = nyTime.format("MMMM Do YYYY");
-nyTimeElement.innerHTML = nyTime.format("h:mm:ss [<small>]A[</small]");
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+
+  let nyElement = document.querySelector("#New-york");
+  let nyDateElement = nyElement.querySelector("#newyork-date");
+  let nyTimeElement = document.querySelector("#newyork-time");
+  let nyTime = moment().tz("America/New_York");
+
+  nyDateElement.innerHTML = nyTime.format("MMMM Do YYYY");
+  nyTimeElement.innerHTML = nyTime.format("h:mm:ss [<small>]A[</small]");
+}
+
+setInterval(updateInterval, 1000);
