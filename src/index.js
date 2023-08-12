8661f5470e4dd2e379235a37091f0ae6b38b2f3c
gloria-tep <tep.gloria18@gmail.com>
2023-08-12 12:00:25 +0100

    updated dropdown list

--- src/index.js
+++ src/index.js
@@ -25,5 +25,24 @@ function updateInterval() {
   nyDateElement.innerHTML = nyTime.format("MMMM Do YYYY");
   nyTimeElement.innerHTML = nyTime.format("h:mm:ss [<small>]A[</small]");
 }
-
 setInterval(updateInterval, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityDateElement = cityTime.format("MMMM Do YYYY");
+  let cityTimeElement = cityTime.format("h:mm:ss [<small>]A[</small]");
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city"> 
+  <div>
+            <h2>${cityName}</h2>
+            <div class="date" id="newyork-date">${cityDateElement}</div>
+          </div>
+          <div class="time" id="newyork-time">${cityTimeElement}
+          </div>`;
+}
+setInterval(updateCity, 1000);
+let citySelectElement = document.querySelector("#city");
+citySelectElement.addEventListener("change", updateCity);
