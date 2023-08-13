6a600a5bd3540760682613728ca99e0ac37aa06a
gloria-tep <tep.gloria18@gmail.com>
2023-08-13 12:11:45 +0100

    changed city name

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 function updateInterval() {
-  let londonElement = document.querySelector("#London");
-  let londonDateElement = londonElement.querySelector("#london-date");
-  let londonTimeElement = document.querySelector("#london-time");
-  let londonTime = moment().tz("Europe/London");
+  let singaporeElement = document.querySelector("#singapore");
+  let singaporeDateElement = singaporeElement.querySelector("#singapore-date");
+  let singaporeTimeElement = document.querySelector("#singapore-time");
+  let singaporeTime = moment().tz("Asia/Singapore");
 
-  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
-  londonTimeElement.innerHTML = londonTime.format(
+  singaporeDateElement.innerHTML = singaporeTime.format("MMMM Do YYYY");
+  singaporeTimeElement.innerHTML = singaporeTime.format(
     "h:mm:ss [<small>]A[</small]"
   );
 
